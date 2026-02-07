873a5e07e25785ce38b638b9fd01a1634789fbaa
Sk Samar <sksamar.de@gmail.com>
2026-02-07 19:41:44 +0100

    dependency removed

--- details/details_presentation/build.gradle.kts
+++ details/details_presentation/build.gradle.kts
@@ -48,27 +48,14 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
 
     implementation(project(":common:common_utls"))
     implementation(project(":details:details_domain"))
-
-    //ratingbar
-    implementation(libs.compose.ratingbar)
-
-    // coil
-    implementation(libs.coil.compose)
-    // ADD THIS ONE:
-    implementation(libs.coil.network.okhttp)
 
     //hilt
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
-
     implementation(libs.androidx.lifecycle.viewmodel.compose)
 }
--- search/search_presentation/build.gradle.kts
+++ search/search_presentation/build.gradle.kts
@@ -48,11 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
 
     implementation(project(":common:common_utls"))
     implementation(project(":search:search_domain"))
@@ -61,7 +57,5 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
-
     implementation(libs.androidx.lifecycle.viewmodel.compose)
-
 }
